3bfddf00ca761e75ffc06a1841efcca7bad5145a
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-15 20:34:38 -0400

    Followed the UML given in the homework assignment. Coffee has inherited the default values of Beverage, added allergens, ingredients, coffeeType, creamerType, strength. Fixed the returns on getCaffeine, getStrength, and getAllergens. Added a protected class with a switch statement getBrewType to return either drip, press, or pod for different coffeeTypes.

--- src/main/java/Coffee.java
+++ src/main/java/Coffee.java
@@ -1,18 +1,49 @@
 public class Coffee extends Beverage {
+    private int caffeine; // x per mg
+    private String strength; // strong, weak, medium, any
+    private String creamerType;
+    private String coffeeType;
+    private String allergens;
+    private String brewType;
 
     public Coffee() {
-        // something here
+        super();
+        allergens = "caffeine";
+        ingredients.add("coffee beans");
+        caffeine = 2;
+        brewType = "drip";
     }
 
     public Coffee(String strength, String coffeeType, String creamerType) {
-        // something here
+        super();
+        this.strength = strength;
+        this.ingredients.add(coffeeType);
+        this.coffeeType = coffeeType;
+        this.ingredients.add(creamerType);
+        this.creamerType = creamerType;
+        this.allergens = "caffeine dairy";
     }
 
     public int getCaffeine() {
-        return 0;
+        return caffeine;
     }
 
     public String getStrength() {
-        return "";
+        return strength;
+    }
+
+    public String getAllergens() {
+        return allergens;
+    }
+
+    protected String getBrewType() {
+        switch(coffeeType.toLowerCase()) {
+            default: case "espresso": case "columbian":
+                return "drip";
+            case "French press":
+                return "press";
+            case "donut shop": case "mocha":
+                return "pod";
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Tea tea = new Tea();
+        Coffee coffee = new Coffee();
 
         System.out.println(tea.cream);
         System.out.println(tea.ingredients);
